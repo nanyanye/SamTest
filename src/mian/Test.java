4c9a8690e00a4146d85f3d566306f904e5536142
nanyanye <nanyan.ye@lcola.cn>
2016-06-02 19:54:43 +0800

    重构

--- src/mian/Test.java
+++ src/mian/Test.java
@@ -2,7 +2,9 @@ package mian;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -27,58 +29,74 @@ public class Test {
 	 *            value 关键字查询出所有满足条件的 key
 	 */
 	public static void main(String[] args) {
+		getData();
+	}
+
+	public static void getData() {
+		Object objYaml = null;
 		try {
-			Object objYaml = new Yaml().load(new FileInputStream(new File("conf/testYaml.yaml")));
-			List<String> list = new ArrayList<String>(Arrays.asList(objYaml.toString().split("url:")));
-			list.stream().filter(url -> url.length() > 1).forEach((u) -> {
-				try {
-					Thread.sleep(10);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-				new Thread(() -> {
-					try {
-						String fileName = System.currentTimeMillis() + "";
-						// guava io
-						ByteStreams.copy(new URL(u).openStream(),
-								new FileOutputStream("E://test/" + fileName + ".txt"));
-						JSONObject object = JSONObject.fromObject(Files.readFirstLine(new File("E://test/" + fileName + ".txt"), StandardCharsets.UTF_8));
-						decodeJSONObject(object);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}).start();
-			});
-		} catch (Exception e) {
+			objYaml = new Yaml().load(new FileInputStream(new File("conf/testYaml.yaml")));
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		List<String> list = new ArrayList<String>(Arrays.asList(objYaml.toString().split("url:")));
+		list.stream().filter(url -> url.length() > 1).forEach((u) -> {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			new Thread(() -> {
+
+				String fileName = System.currentTimeMillis() + "";
+				String classPath = (Test.class.getResource("") + "").substring(6);
+				// guava io
+				try {
+					ByteStreams.copy(new URL(u).openStream(), new FileOutputStream(classPath + fileName + ".txt"));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+				JSONObject object = null;
+				try {
+					object = JSONObject.fromObject(
+							Files.readFirstLine(new File(classPath + fileName + ".txt"), StandardCharsets.UTF_8));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				decodeJSONObject(object);
+
+			}).start();
+		});
 	}
-	//循环遍历json所有信息
-	@SuppressWarnings("unchecked")
-	public static void decodeJSONObject(JSONObject json){  
-        Iterator<String> keys=json.keys();  
-        JSONObject jsonObj=null;  
-        Object o;  
-        String key;  
-        while(keys.hasNext()){  
-            key=keys.next();  
-            //一个个key去重新拿对象
-            o=json.get(key);  
-            //判断是否为JSONObject
-            if(o instanceof JSONObject){  
-            	//是JSONObject 回调
-                 decodeJSONObject((JSONObject)o);  
-              //判断是否为JSONArray
-            }else if(o instanceof JSONArray){ 
-            	//o为JSONArray  
-            	JSONArray os =(JSONArray)o; 
-            	os.stream().forEach((object) ->{
-            		decodeJSONObject((JSONObject)object);
-            	});
-            //以上都不是就是值了
-            }  else{
-            	System.out.println("key:"+key+"---->"+"value:"+o);
-            }
-        }  
-    }  
+
+	// 循环遍历json所有信息
+	@SuppressWarnings({ "unchecked", "unused" })
+	public static void decodeJSONObject(JSONObject json) {
+		Iterator<String> keys = json.keys();
+		JSONObject jsonObj = null;
+		Object o;
+		String key;
+		while (keys.hasNext()) {
+			key = keys.next();
+			// 一个个key去重新拿对象
+			o = json.get(key);
+			// 判断是否为JSONObject
+			if (o instanceof JSONObject) {
+				// 是JSONObject 回调
+				decodeJSONObject((JSONObject) o);
+				// 判断是否为JSONArray
+			} else if (o instanceof JSONArray) {
+				// o为JSONArray
+				JSONArray os = (JSONArray) o;
+				os.stream().forEach((object) -> {
+					decodeJSONObject((JSONObject) object);
+				});
+				// 以上都不是就是value了
+			} else {
+				System.out.println("key:" + key + "---->" + "value:" + o);
+			}
+		}
+	}
 }
